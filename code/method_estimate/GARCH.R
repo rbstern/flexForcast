d6359e9e04e950d1ce046d6c558f7fc4df2748a4
akira <akira.okuno@outlook.com>
2021-04-14 18:04:17 -0300

    GARCH com variaveis exogenas

--- code/method_estimate/GARCH.R
+++ code/method_estimate/GARCH.R
@@ -1,28 +1,47 @@
 GARCH.run=function(ytrain,yeval,ytest,alpha_seq,lags=1,xtrain=NULL,xeval=NULL,xtest=NULL,armaOrder,garchOrder){
   
-  ytrain<<-ytrain
-  yeval<<-yeval
-  ytest<<-ytest
-  
   try({
-  model.garch = ugarchspec(mean.model=list(armaOrder=c(lags,lags)),
-                           variance.model=list(garchOrder=c(lags,lags)),
-                           distribution.model = "std")
-
-
-  model.garch.fit = ugarchfit(data=c(ytrain,yeval,ytest), spec=model.garch,  solver = 'hybrid' ,out.sample = length(ytest))
-  modelfor=ugarchforecast(model.garch.fit, data = ytest, n.ahead = 1,n.roll = length(ytest))
-  
-  quantiles_list = list()
-  #pred=modelfor@forecast$seriesFor[1,1:length(ytest)]
-  
-  for (ii in 1:length(alpha_seq)){
-    q_garch_ii = qnorm(alpha_seq[ii],
-                       mean=modelfor@forecast$seriesFor[1,1:length(ytest)],
-                       sd=modelfor@forecast$sigmaFor[1,1:length(ytest)])
-    quantiles_list[[ii]]=q_garch_ii
-  }
-  
+    
+    if (sum(!grepl("lag",colnames(xtrain)))>0) {
+      model.garch = ugarchspec(mean.model=list(armaOrder=c(2,2),external.regressors = as.matrix(rbind(Xtrain,Xvalid))),
+                               variance.model=list(garchOrder=c(2,2)),
+                               distribution.model = "std")
+      
+      
+      model.garch.fit = ugarchfit(data=c(ytrain,yvalid,ytest), spec=model.garch,  solver = 'hybrid' ,out.sample = length(ytest))
+      modelfor=ugarchforecast(model.garch.fit, data = ytest, n.ahead = 1,n.roll = length(ytest),external.forecasts = Xtest)
+      
+      quantiles_list = list()
+      #pred=modelfor@forecast$seriesFor[1,1:length(ytest)]
+      
+      for (ii in 1:length(alpha_seq)){
+        q_garch_ii = qnorm(alpha_seq[ii],
+                           mean=modelfor@forecast$seriesFor[1,1:length(ytest)],
+                           sd=modelfor@forecast$sigmaFor[1,1:length(ytest)])
+        quantiles_list[[ii]]=q_garch_ii
+        
+      }
+    } else {
+      
+      model.garch = ugarchspec(mean.model=list(armaOrder=c(lags,lags)),
+                               variance.model=list(garchOrder=c(lags,lags)),
+                               distribution.model = "std")
+      
+      
+      model.garch.fit = ugarchfit(data=c(ytrain,yeval,ytest), spec=model.garch,  solver = 'hybrid' ,out.sample = length(ytest))
+      modelfor=ugarchforecast(model.garch.fit, data = ytest, n.ahead = 1,n.roll = length(ytest))
+      
+      quantiles_list = list()
+      pred=modelfor@forecast$seriesFor[1,1:length(ytest)]
+      
+      for (ii in 1:length(alpha_seq)){
+        q_garch_ii = qnorm(alpha_seq[ii],
+                           mean=modelfor@forecast$seriesFor[1,1:length(ytest)],
+                           sd=modelfor@forecast$sigmaFor[1,1:length(ytest)])
+        quantiles_list[[ii]]=q_garch_ii
+      }
+      
+    }
   },silent = TRUE)
   
   if (!exists("modelfor")) {
@@ -45,7 +64,7 @@ GARCH.run=function(ytrain,yeval,ytest,alpha_seq,lags=1,xtrain=NULL,xeval=NULL,xt
   convergence_garch=exists("modelfor")
   
   #plot=matplot(data.frame(ztest,q_garch_05,q_garch_20,q_garch_80,q_garch_95),type = "l")
-
+  
   n_grid = 1000
   z_grid = seq(min(ytrain), max(ytrain), length.out = n_grid)
   z_grid=matrix(z_grid, nrow=length(z_grid),ncol=1)
@@ -53,7 +72,7 @@ GARCH.run=function(ytrain,yeval,ytest,alpha_seq,lags=1,xtrain=NULL,xeval=NULL,xt
   df_quantiles = data.frame(quantiles_list)
   colnames(df_quantiles)=alpha_seq
   if (convergence_garch){
-  return(list(df_quantiles=df_quantiles,garch_model = model.garch.fit,z_grid=z_grid,convergence_garch=convergence_garch))
+    return(list(df_quantiles=df_quantiles,garch_model = model.garch.fit,z_grid=z_grid,convergence_garch=convergence_garch))
   } else{
     return(list(df_quantiles=df_quantiles,garch_model = fit2,z_grid=z_grid,convergence_garch=convergence_garch))
   }
@@ -62,47 +81,48 @@ GARCH.run=function(ytrain,yeval,ytest,alpha_seq,lags=1,xtrain=NULL,xeval=NULL,xt
 
 
 GARCH.pinball_loss = function(garch_output,ytest,alpha_seq){
-
+  
   df = garch_output$df_quantiles
   results = c()
-
+  
   for (i in 1:length(alpha_seq)){
     results = c(results, pinball_loss(ytest,df[,i],alpha_seq[i]))
   }
-
+  
   return(results)
-
+  
 }
 
 GARCH.cde_estimate = function(garch_output,ytest){
+  
+  z_grid = garch_output$z_grid
+  
+  if (garch_output$convergence_garch) {
+    
+    garch_model = garch_output$garch_model
+    modelfor=ugarchforecast(garch_model, data = ytest, n.ahead = 1,n.roll = length(ytest))
+    mean_vector = modelfor@forecast$seriesFor[1,1:length(ytest)]
+    sigma_vector = modelfor@forecast$sigmaFor[1,1:length(ytest)]
+    
+  } else {
+    print("sem convergencia")
+    mean_vector=garch_output$fit2$fitted[(length(garch_output$fit2$fitted)-length(ytest)+1):length(garch_output$fit2$fitted)]
+    sigma_vector=rep(garch_output$fit2$sigma2**(1/2),length(ytest))
+    
+  }
+  
+  n_grid=1000
+  cdes = matrix(0,nrow=length(ytest),ncol=n_grid)
+  
+  for (i in 1:length(ytest)){
+    
+    cdes[i,] = dnorm(z_grid,mean = mean_vector[i],sd=sigma_vector[i])
+    cdes[i,] = cdes[i,]/sum(cdes[i,])
+  }
+  
+  return(cdes)
+}
 
-   z_grid = garch_output$z_grid
-   
-   if (garch_output$convergence_garch) {
-     
-     garch_model = garch_output$garch_model
-     modelfor=ugarchforecast(garch_model, data = ytest, n.ahead = 1,n.roll = length(ytest))
-     mean_vector = modelfor@forecast$seriesFor[1,1:length(ytest)]
-     sigma_vector = modelfor@forecast$sigmaFor[1,1:length(ytest)]
-     
-   } else {
-     
-     mean_vector=garch_output$fit2$fitted[(length(garch_output$fit2$fitted)-length(ytest)+1):length(garch_output$fit2$fitted)]
-     sigma_vector=rep(garch_output$fit2$sigma2**(1/2),length(ytest))
-
-   }
-   
-   n_grid=1000
-   cdes = matrix(0,nrow=length(ytest),ncol=n_grid)
-   
-   for (i in 1:length(ytest)){
-     cdes[i,] = dnorm(z_grid,mean = mean_vector[i],sd=sigma_vector[i])
-     cdes[i,] = cdes[i,]/sum(cdes[i,])
-   }
-
-   return(cdes)
- }
- 
 garch_training = function(train_valid_test_sets,alpha_seq)
 {
   ytrain=train_valid_test_sets$ytrain
--- code/test_methods.R
+++ code/test_methods.R
@@ -8,7 +8,7 @@ test_methods = function(train_valid_test_sets,alpha_seq,lags,params){
   p_train=params$p_train
   n_cores_flexcode=params$n_cores_flexcode
   
-  qar_training2 = partial(qar_training,L=lags)
+  qar_training = partial(qar_training,L=lags)
   flexcode_rf   = partial(flexcode_training,p_train=p_train,regressionFunction=regressionFunction.Forest,lags_y=lags,n_cores=n_cores_flexcode)
   flexcode_xgb  = partial(flexcode_training,p_train=p_train,regressionFunction=regressionFunction.XGBoost,lags_y=lags,n_cores=n_cores_flexcode)
   
