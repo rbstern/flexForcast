81bd2fae13ca4a4a7acee5b57121d676cf9e98b8
rizbicki <rafaelizbicki@gmail.com>
2021-04-26 17:56:15 -0300

    sin

--- code/auxiliary/combine_tables.R
+++ code/auxiliary/combine_tables.R
@@ -66,7 +66,7 @@ read_quantile_loss <- function(arqs)
 
 }
 
-all_tables <- function(arqs,n)
+all_tables <- function(arqs,n,which_quantiles=NULL,remove="NONAME")
 {
   arqs_subset <- arqs %>% 
     str_subset(.,as.character(n))
@@ -77,9 +77,47 @@ all_tables <- function(arqs,n)
     read_quantile_loss() %>% 
     convert_to_mean_se()
   
+  # add_header_above(c(" ", "Group 1" = 5, "Group 2" = 6)) 
+  # 
+  # n_quantiles <- length(unique(data_quantile$quantile)) 
+  # labels <- rep(n_quantiles,each=length(unique(data_quantile$setting)))
+  # names(labels) <- unique(data_quantile$setting)
+  #   
+  # rows <- (length(labels)-1)%/%2
+  # new_table <- t(data_quantile)[-1,1:(2*n_quantiles)]
+  # ll <- 2*n_quantiles
+  # for(ii in 1:rows)
+  # {
+  #   if(ncol(t(data_quantile))< ll+2*n_quantiles)
+  #   {
+  #     ll <- ll-n_quantiles
+  #   }
+  #   new_aux <- t(data_quantile)[-1,(ll+1):(ll+2*n_quantiles)]
+  #   new_table <- rbind(new_table,new_aux)
+  #   ll <- ll+2*n_quantiles
+  # }
+  # 
+  # table_quantile <- kbl(new_table, booktabs = T,"latex", align = "c", linesep = '') 
+  # for(ii in 1:rows)
+  # {
+  #   table_quantile <- table_quantile %>% 
+  #     add_header_above(c("",labels[])) 
+  # }
+  # 
+  # %>%
+  #   add_header_above(c("",labels)) %>% 
+  #   kable_styling(font_size = 7) %>% 
+  #   print()
+  # 
   
+  if(is.null(which_quantiles))
+  {
+    which_quantiles <- unique(data_quantile$quantile)
+  }
   
-  kbl(data_quantile, booktabs = T,"latex", align = "c", linesep = '') %>%
+  kbl(data_quantile %>% 
+       filter(quantile %in%which_quantiles,!str_detect(setting,remove)), 
+      booktabs = T,"latex", align = "c", linesep = '') %>%
     collapse_rows(1:2, row_group_label_position = 'stack')%>% 
     kable_styling(font_size = 7) %>% 
     print()
@@ -96,6 +134,6 @@ decimal <- 3
 
 folder_files <- "../results/processed/"
 arqs <- list.files(folder_files,full.names=TRUE)
-all_tables(arqs,1000)
-all_tables(arqs,5000)
+all_tables(arqs,1000,which_quantiles = c(0.05,0.2,0.5),remove = "BERNOULLI")
+all_tables(arqs,5000,which_quantiles = c(0.05,0.2,0.5),remove = "BERNOULLI")
 
