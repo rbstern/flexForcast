0474d744904431302504ce7dfd091958e500638b
rizbicki <rafaelizbicki@gmail.com>
2021-05-13 22:11:01 -0300

    function to plot

--- code/auxiliary/combine_tables.R
+++ code/auxiliary/combine_tables.R
@@ -59,23 +59,47 @@ read_quantile_loss <- function(arqs)
     mutate(content=purrr::map(arqs, 
                               readRDS) %>% 
              lapply(function(xx){
-               return(data.frame(statistic=cbind(c("mean","se")),
+               return(data.frame(statistic=c(rep("mean",nrow(xx$pbloss$mean)),
+                                             rep("se",nrow(xx$pbloss$se))),
                                  rbind(xx$pbloss$mean,xx$pbloss$se)))
              })) %>% 
     unnest()
-
 }
 
-all_tables <- function(arqs,n)
+all_tables <- function(arqs,n,which_quantiles,which_settings,methods_remove)
 {
   arqs_subset <- arqs %>% 
     str_subset(.,as.character(n))
+  
+  names_files <- basename(arqs_subset)
+  
+  if(!is.null(which_settings))
+  {
+    arqs_subset <- arqs_subset[names_files %>% 
+                                 str_detect(.,paste(which_settings, collapse="|"))]
+  }
+  
   data_cde <- arqs_subset %>% 
     read_cde_loss() %>% 
     convert_to_mean_se()
   data_quantile <- arqs_subset %>% 
     read_quantile_loss() %>% 
-    convert_to_mean_se()
+    convert_to_mean_se() 
+  
+  if(!is.null(which_quantiles))
+  {
+    data_quantile <- data_quantile %>% 
+      filter(quantile%in%which_quantiles)
+  }
+  
+  if(!is.null(methods_remove))
+  {
+    data_quantile <- data_quantile %>% 
+      select(!contains(methods_remove))
+    
+    data_cde <- data_cde %>% 
+      select(!contains(methods_remove))
+  }
   
   
   
@@ -92,10 +116,78 @@ all_tables <- function(arqs,n)
   
 }
 
-decimal <- 3
 
+
+all_plots <- function(arqs,which_quantiles,which_settings,methods_remove)
+{
+  arqs_subset <- arqs 
+  
+  names_files <- basename(arqs_subset)
+  
+  if(!is.null(which_settings))
+  {
+    arqs_subset <- arqs_subset[names_files %>% 
+                                 str_detect(.,paste(which_settings, collapse="|"))]
+  }
+  
+  data_cde <- arqs_subset %>% 
+    read_cde_loss() %>% 
+    mutate(n=as.numeric(stringi::stri_extract_last_regex(setting, "\\d{4}"))) %>% 
+    mutate(setting=str_replace_all(setting, "[:digit:]", ""))
+  
+  
+  data_quantile <- arqs_subset %>% 
+    read_quantile_loss()  %>% 
+    mutate(n=as.numeric(stringi::stri_extract_last_regex(setting, "\\d{4}")))%>% 
+    mutate(setting=str_replace_all(setting, "[:digit:]", ""))
+  
+  if(!is.null(which_quantiles))
+  {
+    data_quantile <- data_quantile %>% 
+      filter(quantile%in%which_quantiles)
+  }
+  
+  if(!is.null(methods_remove))
+  {
+    data_quantile <- data_quantile %>% 
+      select(!contains(methods_remove))
+    
+    data_cde <- data_cde %>% 
+      select(!contains(methods_remove))
+  }
+  
+  data_cde <- data_cde %>% 
+    pivot_longer(NNKCDE:FLEX_RF,names_to = "method",
+                 values_to = "CDE",names_repair = "unique") %>% 
+    pivot_wider(names_from = statistic,values_from = CDE) %>% 
+    mutate(quantile="CDE")
+  
+  data_quantile <- data_quantile %>% 
+    pivot_longer(QAR:FLEX_RF,names_to = "method",
+                 values_to = "Pinball",names_repair = "unique") %>% 
+    pivot_wider(names_from = statistic,values_from = Pinball) %>% 
+    mutate(quantile=as.character(quantile))
+  
+  data_all <- full_join(data_cde,data_quantile)
+  
+  ggplot(data_all)+
+    geom_line(aes(x=n,y=mean,color=method))+
+    facet_wrap(setting~quantile, scales="free",ncol = 4)
+  
+}
+
+decimal <- 3
+which_quantiles <- c(0.5,0.8,0.95)
+which_settings<- c("^AR_1","^ARMA_11",
+                   "^ARMAJUMP","^ARMATJUMP",
+                   "^JUMPDIFFUSION","^SINE")
+methods_remove <- "XGB"
 folder_files <- "../results/processed/"
 arqs <- list.files(folder_files,full.names=TRUE)
-all_tables(arqs,1000)
-all_tables(arqs,5000)
+
+
+
+all_tables(arqs,1000,which_quantiles,which_settings,methods_remove)
+
+all_tables(arqs,5000,which_quantiles,which_settings,methods_remove)
 
