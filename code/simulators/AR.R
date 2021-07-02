5e2a0ce9891a30a32f08a01861c9c5e024d814b4
akira <akira.okuno@outlook.com>
2021-07-02 18:29:48 -0300

    EXPERIMENTOS LAG -AR E SINE LAGGED

--- code/simulators/AR.R
+++ code/simulators/AR.R
@@ -87,14 +87,62 @@ for (n_obs in n_obs_list) {
 
 #####################################################################################
 
-# n_obs=1000
-# cdelosses = list()
-# 
-# for (lags in c(1,3,5,10,15)){
-#   
-#   this_simulator = partial(ar_simulator,n_obs=n_obs,coeffs=coeffs)
-#   this_loss = simulation_run(this_simulator, lags=lags, n_iter = n_iter, mini=TRUE)
-#   this_cdeloss = this_loss$cdeloss
-#   cdelosses[[as.character(lags)]]=this_cdeloss
-#   
-# }
+run=TRUE
+
+if (run==TRUE){
+  
+  n_obs=1000
+  cdelosses = list()
+  
+  for (lags in c(1,3,5,10,15)){
+    
+    this_simulator = partial(ar_simulator,n_obs=n_obs,coeffs=coeffs)
+    this_loss = simulation_run(this_simulator, lags=lags, n_iter = n_iter)
+    this_cdeloss = this_loss$cdeloss
+    cdelosses[[as.character(lags)]]=this_cdeloss
+    
+  }
+  
+  for (i in 1:length(cdelosses)){
+    dfi = cdelosses[[i]]
+    for (j in 1:ncol(cdelosses[[i]]))
+      dfi[,j][is.na(dfi[,j])] <- max(dfi[,j], na.rm = TRUE)
+    cdelosses[[i]] = dfi
+  }
+  
+  df = matrix(nrow=6,ncol=3)
+  dfse = matrix(nrow=6,ncol=3)
+  for (i in 1:6){
+    losses_i=colMeans(cdelosses[[i]])
+    losses_se = apply(cdelosses[[i]], 2,sd)
+    df[i,]=losses_i
+    dfse[i,]=losses_se
+  }
+  colnames(df)=c("GARCH","NNKCDE","FLEX_RF")
+  colnames(dfse)=c("GARCHse","NNKCDEse","FLEX_RFse")
+  df = cbind(df,dfse)
+  df=as.data.frame(df)
+  
+  df['GARCH_low']=df['GARCH']-df['GARCHse']
+  df['GARCH_up']=df['GARCH']+df['GARCHse']
+  df['NNKCDE_low']=df['NNKCDE']-df['NNKCDEse']
+  df['NNKCDE_up']=df['NNKCDE']+df['NNKCDEse']
+  df['FLEX_RF_low']=df['FLEX_RF']-df['FLEX_RFse']
+  df['FLEX_RF_up']=df['FLEX_RF']+df['FLEX_RFse']
+  
+  library(ggplot2)
+  
+  x= c(1,3,5,10,15,20,25,50,100)
+  ggplot(data=df, aes(x=x))+
+    geom_ribbon(aes(ymin=GARCH_low,ymax=GARCH_up),alpha=0.25)+
+    geom_line(aes(y=GARCH,color="blue"),size=0.9)+
+    geom_ribbon(aes(ymin=NNKCDE_low,ymax=NNKCDE_up),alpha=0.25)+
+    geom_line(aes(y=NNKCDE,color="red"),size=0.9)+
+    geom_ribbon(aes(ymin=FLEX_RF_low,ymax=FLEX_RF_up),alpha=0.25)+
+    geom_line(aes(y=FLEX_RF,color="green"),size=0.9)+
+    scale_color_discrete(name = "Methods", labels = c("GARCH","FLEXCODE-RF","NNK-CDE"))+
+    ylab("CDE loss") + xlab("No. of lags") +
+    scale_x_continuous(breaks=x)
+  
+  
+}
--- code/simulators/SINE.R
+++ code/simulators/SINE.R
@@ -48,75 +48,78 @@ for (n_obs in n_obs_list) {
 }
 
 #############################################################################
-
-n_obs=1000
-cdelosses = list()
-
-for (lags in c(1,3,5,10,15,20,25,50,100)){
-
-  this_simulator = partial(sine_ar_simulator,n_obs=n_obs)
-  this_loss = simulation_run(this_simulator, lags=lags, n_iter = n_iter)
-  this_cdeloss = this_loss$cdeloss
-  cdelosses[[as.character(lags)]]=this_cdeloss
-
+run=FALSE
+if (run == TRUE){
+  n_obs=1000
+  cdelosses = list()
+  
+  for (lags in c(1,3,5,10,15,20,25,50,100)){
+    
+    this_simulator = partial(sine_ar_simulator,n_obs=n_obs)
+    this_loss = simulation_run(this_simulator, lags=lags, n_iter = n_iter)
+    this_cdeloss = this_loss$cdeloss
+    cdelosses[[as.character(lags)]]=this_cdeloss
+    
+  }
+  
+  for (i in 1:length(cdelosses)){
+    dfi = cdelosses[[i]]
+    for (j in 1:ncol(cdelosses[[i]]))
+      dfi[,j][is.na(dfi[,j])] <- max(dfi[,j], na.rm = TRUE)
+    cdelosses[[i]] = dfi
+  }
+  
+  df = matrix(nrow=6,ncol=3)
+  dfse = matrix(nrow=6,ncol=3)
+  for (i in 1:6){
+    losses_i=colMeans(cdelosses[[i]])
+    losses_se = apply(cdelosses[[i]], 2,sd)
+    df[i,]=losses_i
+    dfse[i,]=losses_se
+  }
+  colnames(df)=c("GARCH","NNKCDE","FLEX_RF")
+  colnames(dfse)=c("GARCHse","NNKCDEse","FLEX_RFse")
+  df = cbind(df,dfse)
+  df=as.data.frame(df)
+  
+  df['GARCH_low']=df['GARCH']-df['GARCHse']
+  df['GARCH_up']=df['GARCH']+df['GARCHse']
+  df['NNKCDE_low']=df['NNKCDE']-df['NNKCDEse']
+  df['NNKCDE_up']=df['NNKCDE']+df['NNKCDEse']
+  df['FLEX_RF_low']=df['FLEX_RF']-df['FLEX_RFse']
+  df['FLEX_RF_up']=df['FLEX_RF']+df['FLEX_RFse']
+  
+  library(ggplot2)
+  
+  x= c(1,3,5,10,15,20,25,50,100)
+  ggplot(data=df, aes(x=x))+
+    geom_ribbon(aes(ymin=GARCH_low,ymax=GARCH_up),alpha=0.25)+
+    geom_line(aes(y=GARCH,color="blue"),size=0.9)+
+    geom_ribbon(aes(ymin=NNKCDE_low,ymax=NNKCDE_up),alpha=0.25)+
+    geom_line(aes(y=NNKCDE,color="red"),size=0.9)+
+    geom_ribbon(aes(ymin=FLEX_RF_low,ymax=FLEX_RF_up),alpha=0.25)+
+    geom_line(aes(y=FLEX_RF,color="green"),size=0.9)+
+    scale_color_discrete(name = "Methods", labels = c("GARCH","FLEXCODE-RF","NNK-CDE"))+
+    ylab("CDE loss") + xlab("No. of lags") +
+    scale_x_continuous(breaks=x)
 }
 
-for (i in 1:length(cdelosses)){
-  dfi = cdelosses[[i]]
-  for (j in 1:ncol(cdelosses[[i]]))
-    dfi[,j][is.na(dfi[,j])] <- max(dfi[,j], na.rm = TRUE)
-  cdelosses[[i]] = dfi
-}
-
-df = matrix(nrow=6,ncol=3)
-dfse = matrix(nrow=6,ncol=3)
-for (i in 1:6){
-  losses_i=colMeans(cdelosses[[i]])
-  losses_se = apply(cdelosses[[i]], 2,sd)
-  df[i,]=losses_i
-  dfse[i,]=losses_se
-}
-colnames(df)=c("GARCH","NNKCDE","FLEX_RF")
-colnames(dfse)=c("GARCHse","NNKCDEse","FLEX_RFse")
-df = cbind(df,dfse)
-df=as.data.frame(df)
-
-df['GARCH_low']=df['GARCH']-df['GARCHse']
-df['GARCH_up']=df['GARCH']+df['GARCHse']
-df['NNKCDE_low']=df['NNKCDE']-df['NNKCDEse']
-df['NNKCDE_up']=df['NNKCDE']+df['NNKCDEse']
-df['FLEX_RF_low']=df['FLEX_RF']-df['FLEX_RFse']
-df['FLEX_RF_up']=df['FLEX_RF']+df['FLEX_RFse']
-
-library(ggplot2)
-
-x= c(1,3,5,10,15,20)
-ggplot(data=df, aes(x=x))+
-  geom_ribbon(aes(ymin=GARCH_low,ymax=GARCH_up),alpha=0.25)+
-  geom_line(aes(y=GARCH,color="blue"),size=0.9)+
-  geom_ribbon(aes(ymin=NNKCDE_low,ymax=NNKCDE_up),alpha=0.25)+
-  geom_line(aes(y=NNKCDE,color="red"),size=0.9)+
-  geom_ribbon(aes(ymin=FLEX_RF_low,ymax=FLEX_RF_up),alpha=0.25)+
-  geom_line(aes(y=FLEX_RF,color="green"),size=0.9)+
-  scale_color_discrete(name = "Methods", labels = c("GARCH","FLEXCODE-RF","NNK-CDE"))+
-  ylab("CDE loss") + xlab("No. of lags") +
-  scale_x_continuous(breaks=x)
 
 
-plot(c(1,3,5,10,15,20,25,50,100),df$GARCH,type='b',bty="l",xlab="No. of Lags",ylab="CDE loss",ylim=c(lmin-0.1*d,1),col=rgb(0.2,0.4,0.1,0.7) , lwd=3 , pch=17, xaxt="none")
-lines(c(1,3,5,10,15,20,25,50,100),df$`NNK-CDE`,type="b",col=rgb(0.8,0.4,0.1,0.7) , lwd=3 , pch=19)
-lines(c(1,3,5,10,15,20,25,50,100),df$`FLEX-RF`,type="b",col=rgb(0.8,0.7,0.1,0.7) , lwd=3 , pch=15)
-
-axis(1, c(1,3,5,10,15,20,25,50,100),las=2, cex.axis=0.8, font=2)
-legend("right",
-       legend = c("GARCH","NNK-CDE","FLEX-RF"),
-       col = c(rgb(0.2,0.4,0.1,0.7),
-               rgb(0.8,0.4,0.1,0.7),
-               rgb(0.8,0.7,0.1,0.7)),
-       pch = c(17,19,15),
-       bty = "n",
-       pt.cex = 1.5,
-       cex = 0.6,
-       text.col = "black",
-       horiz = F ,
-       inset = c(0.1, 0.1))
+# plot(c(1,3,5,10,15,20,25,50,100),df$GARCH,type='b',bty="l",xlab="No. of Lags",ylab="CDE loss",ylim=c(lmin-0.1*d,1),col=rgb(0.2,0.4,0.1,0.7) , lwd=3 , pch=17, xaxt="none")
+# lines(c(1,3,5,10,15,20,25,50,100),df$`NNK-CDE`,type="b",col=rgb(0.8,0.4,0.1,0.7) , lwd=3 , pch=19)
+# lines(c(1,3,5,10,15,20,25,50,100),df$`FLEX-RF`,type="b",col=rgb(0.8,0.7,0.1,0.7) , lwd=3 , pch=15)
+# 
+# axis(1, c(1,3,5,10,15,20,25,50,100),las=2, cex.axis=0.8, font=2)
+# legend("right",
+#        legend = c("GARCH","NNK-CDE","FLEX-RF"),
+#        col = c(rgb(0.2,0.4,0.1,0.7),
+#                rgb(0.8,0.4,0.1,0.7),
+#                rgb(0.8,0.7,0.1,0.7)),
+#        pch = c(17,19,15),
+#        bty = "n",
+#        pt.cex = 1.5,
+#        cex = 0.6,
+#        text.col = "black",
+#        horiz = F ,
+#        inset = c(0.1, 0.1))
